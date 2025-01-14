244b6e6c4ff8b302cd8a23dcb4c92ae8c0ea65dc
Ruben <ruben@live.dk>
2025-01-14 22:50:10 +0100

    Image effect updates

--- src/PicView.Avalonia/ImageEffects/ImageEffectConfig.cs
+++ src/PicView.Avalonia/ImageEffects/ImageEffectConfig.cs
@@ -2,15 +2,17 @@
 
 namespace PicView.Avalonia.ImageEffects;
 
-public struct ImageEffectConfig(Percentage brightness, Percentage contrast, double sketchStrokeWidth, int posterizeLevel, bool negative, bool blackAndWhite, bool oldMovie)
+public struct ImageEffectConfig(Percentage brightness, Percentage contrast, double sketchStrokeWidth, double blurRadius, int posterizeLevel, bool negative, bool blackAndWhite, bool oldMovie)
 {
     public Percentage Brightness = brightness;
     public Percentage Contrast = contrast;
     
     public double SketchStrokeWidth = sketchStrokeWidth;
+    
     public int PosterizeLevel = posterizeLevel;
     
     public bool Negative = negative;
     public bool BlackAndWhite = blackAndWhite;
     public bool OldMovie = oldMovie;
+    public double BlurRadius = blurRadius;
 }
--- src/PicView.Avalonia/Views/EffectsView.axaml.cs
+++ src/PicView.Avalonia/Views/EffectsView.axaml.cs
@@ -3,49 +3,47 @@ using ImageMagick;
 using PicView.Avalonia.ImageEffects;
 using PicView.Avalonia.ImageHandling;
 using PicView.Avalonia.ViewModels;
+using PicView.Core.FileHandling;
 using Timer = System.Timers.Timer;
 
 namespace PicView.Avalonia.Views;
 
 public partial class EffectsView : UserControl
 {
+    private CancellationTokenSource? _cancellationTokenSource;
     private ImageEffectConfig _config;
-    
+
     private Timer? _debounceTimer;
-    
+    private ImageEffectConfig _prevConfig;
+
     public EffectsView()
     {
         InitializeComponent();
-        
+
         Loaded += (_, _) =>
         {
-            ResetContrastBtn.Click += delegate
+            PointerPressed += (_, e) =>
             {
-                ContrastSlider.Value = 0;
+                if (e.GetCurrentPoint(this).Properties.IsRightButtonPressed)
+                {
+                    // Context menu doesn't want to be opened normally
+                    ContextMenu.Open();
+                }
             };
-            ResetBrightnessBtn.Click += delegate
-            {
-                BrightnessSlider.Value = 0;
-            };
-            ResetPencilSketchBtn.Click += delegate
-            {
-                PencilSketchSlider.Value = 0;
-            };
-            ResetPencilSketchBtn.Click += delegate
-            {
-                PencilSketchSlider.Value = 0;
-            };
-            ResetPosterizeSketchBtn.Click += delegate
-            {
-                PosterizeSlider.Value = 0;
-            };
-            
 
             if (DataContext is not MainViewModel vm)
             {
                 return;
             }
-            
+
+            ClearEffectsItem.Click += async delegate { await RemoveEffects(vm); };
+            ResetContrastBtn.Click += delegate { ContrastSlider.Value = 0; };
+            ResetBrightnessBtn.Click += delegate { BrightnessSlider.Value = 0; };
+            ResetPencilSketchBtn.Click += delegate { PencilSketchSlider.Value = 0; };
+            ResetPencilSketchBtn.Click += delegate { PencilSketchSlider.Value = 0; };
+            ResetPosterizeSketchBtn.Click += delegate { PosterizeSlider.Value = 0; };
+            ResetBlurBtn.Click += delegate { BlurSlider.Value = 0; };
+
             _debounceTimer = new Timer { Interval = 300, AutoReset = false };
             _debounceTimer.Elapsed += async (_, _) => await ApplyEffectsDebounced(vm).ConfigureAwait(false);
 
@@ -54,35 +52,41 @@ public partial class EffectsView : UserControl
                 _config.Brightness = new Percentage(e.NewValue);
                 DebounceSliderChange();
             };
-            
+
             ContrastSlider.ValueChanged += (_, e) =>
             {
                 _config.Contrast = new Percentage(e.NewValue);
                 DebounceSliderChange();
             };
-            
+
             PencilSketchSlider.ValueChanged += (_, e) =>
             {
                 _config.SketchStrokeWidth = e.NewValue;
                 DebounceSliderChange();
             };
-            
+
             PosterizeSlider.ValueChanged += (_, e) =>
             {
                 var newValue = (int)e.NewValue;
                 _config.PosterizeLevel = newValue is 1 ? 2 : newValue;
                 DebounceSliderChange();
             };
-            
+
+            BlurSlider.ValueChanged += (_, e) =>
+            {
+                _config.BlurRadius = e.NewValue;
+                DebounceSliderChange();
+            };
+
             BlackAndWhiteToggleButton.Click += async (_, _) =>
             {
                 if (!BlackAndWhiteToggleButton.IsChecked.HasValue)
                 {
                     return;
                 }
-                
+
                 _config.BlackAndWhite = BlackAndWhiteToggleButton.IsChecked.Value;
-                await ApplyEffects(vm);
+                await ApplyEffectsDebounced(vm);
             };
 
             NegativeToggleButton.Click += async (_, _) =>
@@ -90,11 +94,10 @@ public partial class EffectsView : UserControl
                 if (!NegativeToggleButton.IsChecked.HasValue)
                 {
                     return;
-
                 }
 
                 _config.Negative = NegativeToggleButton.IsChecked.Value;
-                await ApplyEffects(vm);
+                await ApplyEffectsDebounced(vm);
             };
 
             OldMovieToggleButton.Click += async (_, _) =>
@@ -105,11 +108,11 @@ public partial class EffectsView : UserControl
                 }
 
                 _config.OldMovie = OldMovieToggleButton.IsChecked.Value;
-                await ApplyEffects(vm);
+                await ApplyEffectsDebounced(vm);
             };
         };
     }
-    
+
     private void DebounceSliderChange()
     {
         _debounceTimer.Stop();
@@ -118,73 +121,104 @@ public partial class EffectsView : UserControl
 
     private async Task ApplyEffectsDebounced(MainViewModel vm)
     {
-        // Your image processing logic here
-        await ApplyEffects(vm);
+        if (_cancellationTokenSource is not null)
+        {
+            await _cancellationTokenSource.CancelAsync().ConfigureAwait(false);
+
+        }
+        _cancellationTokenSource = new CancellationTokenSource();
+        await ApplyEffects(vm, _config, _cancellationTokenSource.Token).ConfigureAwait(false);
     }
 
-    private async Task ApplyEffects(MainViewModel vm)
+    private static async Task ApplyEffects(MainViewModel vm, ImageEffectConfig config, CancellationToken cancellationToken)
     {
-        var fileInfo = vm.FileInfo;
-
-        using var magick = new MagickImage();
-        if (fileInfo.Length >= 2147483648)
+        try
         {
-            // Fixes "The file is too long. This operation is currently limited to supporting files less than 2 gigabytes in size."
-            // ReSharper disable once MethodHasAsyncOverload
-            magick.Read(fileInfo);
-        }
-        else
-        {
-            await magick.ReadAsync(fileInfo).ConfigureAwait(false);
-        }
-        magick.BrightnessContrast(_config.Brightness, _config.Contrast);
-        magick.BackgroundColor = MagickColors.Transparent;
-        magick.Settings.BackgroundColor = MagickColors.Transparent;
-        magick.Settings.FillColor = MagickColors.Transparent;
-        if (_config.Negative)
-        {
-            magick.Negate();
-        }
-
-        if (_config.BlackAndWhite)
-        {
-            magick.Grayscale();
-        }
-
-        if (_config.OldMovie)
-        {
-            // 1. Apply sepia tone
-            magick.SepiaTone(new Percentage(80));
-
-            // 2. Add noise
-            magick.AddNoise(NoiseType.MultiplicativeGaussian);
-
-            var random = new Random();
-            
-            // 3. Add vertical bands (simulate scratches)
-            for (var i = 0; i < magick.Width; i += random.Next(1,50))
+            await Task.Run(async () =>
             {
-                using var band = new MagickImage(new MagickColor("#3E382A"), (uint)random.Next(1,3), magick.Height);
-                band.Evaluate(Channels.Alpha, EvaluateOperator.Set, 0.2); // semi-transparent
-                magick.Composite(band, i, 0, CompositeOperator.Over);
-            }
-        }
+                var fileInfo = vm.FileInfo;
+                using var magick = new MagickImage();
+                if (fileInfo.Length >= 2147483648)
+                {
+                    await using var filestream = FileHelper.GetOptimizedFileStream(fileInfo);
+                    // Fixes "The file is too long. This operation is currently limited to supporting files less than 2 gigabytes in size."
+                    // ReSharper disable once MethodHasAsyncOverload
+                    // ReSharper disable once MethodHasAsyncOverloadWithCancellation
+                    magick.Read(filestream);
+                }
+                else
+                {
+                    await magick.ReadAsync(fileInfo, cancellationToken).ConfigureAwait(false);
+                }
 
-        if (_config.SketchStrokeWidth is not 0)
-        {
-            magick.Charcoal(_config.SketchStrokeWidth, 0);
+                cancellationToken.ThrowIfCancellationRequested();
+                
+                magick.BrightnessContrast(config.Brightness, config.Contrast);
+                magick.BackgroundColor = MagickColors.Transparent;
+                magick.Settings.BackgroundColor = MagickColors.Transparent;
+                magick.Settings.FillColor = MagickColors.Transparent;
+
+                if (config.Negative)
+                {
+                    magick.Negate();
+                }
+
+                if (config.BlackAndWhite)
+                {
+                    magick.Grayscale();
+                }
+
+                if (config.OldMovie)
+                {
+                    // 1. Apply sepia tone
+                    magick.SepiaTone(new Percentage(80));
+
+                    // 2. Add noise
+                    magick.AddNoise(NoiseType.MultiplicativeGaussian);
+
+                    var random = new Random();
+
+                    // 3. Add vertical bands (simulate scratches)
+                    for (var i = 0; i < magick.Width; i += random.Next(1, 50))
+                    {
+                        using var band = new MagickImage(new MagickColor("#3E382A"), (uint)random.Next(1, 3),
+                            magick.Height);
+                        band.Evaluate(Channels.Alpha, EvaluateOperator.Set, 0.2); // semi-transparent
+                        magick.Composite(band, i, 0, CompositeOperator.Over);
+                    }
+                }
+
+                if (config.SketchStrokeWidth is not 0)
+                {
+                    magick.Charcoal(config.SketchStrokeWidth,  3);
+                }
+
+                if (config.PosterizeLevel is not 0)
+                {
+                    magick.Posterize(config.PosterizeLevel);
+                }
+
+                if (config.BlurRadius is not 0)
+                {
+                    magick.MotionBlur(0, config.BlurRadius,0);
+                }
+                
+                cancellationToken.ThrowIfCancellationRequested();
+
+                var bitmap = magick.ToWriteableBitmap();
+                vm.ImageSource = bitmap;
+            }, cancellationToken).ConfigureAwait(false);
         }
-        
-        if (_config.PosterizeLevel is not 0)
+        catch (Exception e)
         {
-            magick.Posterize(_config.PosterizeLevel);
+#if DEBUG
+            Console.WriteLine(e);
+#endif
         }
-        
-        var bitmap = magick.ToWriteableBitmap();
-        vm.ImageSource = bitmap;
     }
-    
-    private static async Task RemoveEffects(MainViewModel vm)
+
+
+    private async Task RemoveEffects(MainViewModel vm)
     {
         using var magick = new MagickImage();
         await magick.ReadAsync(vm.FileInfo.FullName).ConfigureAwait(false);
@@ -193,5 +227,6 @@ public partial class EffectsView : UserControl
         magick.Settings.FillColor = MagickColors.Transparent;
         var bitmap = magick.ToWriteableBitmap();
         vm.ImageSource = bitmap;
+        _config = new ImageEffectConfig();
     }
 }
